33e7f95ab75abf7cf64c3a05312a9020423501f5
Sreeharivs1983 <sreeharivs69@gmail.com>
2026-02-26 01:50:50 -0500

    User Login with JWT

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -29,9 +29,36 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+      return res.status(400).json({ message: "Username and password required" });
+    }
+  
+    if (!authenticatedUser(username, password)) {
+      return res.status(403).json({ message: "Invalid Login. Check username and password" });
+    }
+  
+    let accessToken = jwt.sign(
+      {
+        data: username
+      },
+      "access",
+      { expiresIn: 60 * 60 }
+    );
+  
+    req.session.authorization = {
+      accessToken
+    };
+  
+    return res.status(200).json({
+      message: "User successfully logged in",
+      token: accessToken
+    });
+  
+  });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
